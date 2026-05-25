86b74975ed4e1c1b1bf7723a20f813a3f0066f74
ZiyanRana <mziyanrana@gmail.com>
2026-05-25 22:55:44 +0500

    Working on project: Backend

--- Backend/src/controllers/transaction.controller.js
+++ Backend/src/controllers/transaction.controller.js
@@ -8,24 +8,43 @@ export const createTransaction = (req, res) => {
         return res.status(400).json({ message: 'Some required fields are missing, cannot proceed!' });
     }
 
-    const senderAccount = accountModel.findOne({ _id: sender });
-    const recieverAccount = accountModel.findOne({ _id: reciever });
+    try {
+        // check if data is valid
+        const senderAccount = accountModel.findOne({ _id: sender });
+        const recieverAccount = accountModel.findOne({ _id: reciever });
 
-    if (!senderAccount) {
-        return res.status(400).json({ message: 'Sender account not found!' });
+        if (!senderAccount) {
+            return res.status(400).json({ message: 'Sender account not found!' });
+        }
+        if (!recieverAccount) {
+            return res.status(400).json({ message: 'Reciever account not found!' });
+        }
+
+        // check for duplicate transactions
+        const isTransactionRunning = transactionModel.findOne({ 
+            idempotencyKey: idempotencyKey
+        }).select('_id date status');
+
+        if (isTransactionRunning) {
+            return res.status(409).json({ 
+                message: 'A transaction with the same idempotency key is already in progress!',
+                transaction: isTransactionRunning
+            });
+        }
+
+        // check if accounts are active
+        if (senderAccount.status !== 'active') {
+            return res.status(400).json({ message: 'Cannot proceed, the sender account is not active!' });
+        }
+        if (recieverAccount.status !== 'active') {
+            return res.status(400).json({ message: 'Cannot proceed, the reciever account is not active!' });
+        }
+
+        // check if the sender has enough balance
+        
     }
-    if (!recieverAccount) {
-        return res.status(400).json({ message: 'Reciever account not found!' });
-    }
-
-    const isTransactionRunning = transactionModel.findOne({ 
-        idempotencyKey: idempotencyKey
-    });
-
-    if (isTransactionRunning) {
-        return res.status(409).json({ 
-            message: 'A transaction with the same idempotency key is already in progress!',
-            transaction: isTransactionRunning
-         });
+    catch (error) {
+        console.error('Error creating transaction: ', error);
+        return res.status(500).json({ message: 'Please try again' });
     }
 }
