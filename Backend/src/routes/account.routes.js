651088c1d4af94b515d678f751f60a7bee918039
ZiyanRana <mziyanrana@gmail.com>
2026-05-25 16:47:54 +0500

    Working on project: Backend

--- Backend/src/routes/account.routes.js
+++ Backend/src/routes/account.routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { getAccount, getUserAccounts, createAccount, updateAccount, deleteAccount } from '../controllers/account.controller.js';
+import authMiddleware from '../middlewares/auth.middleware.js';
 
 const accountRouter = Router();
 
 // Path: /api/v1/accounts
-accountRouter.get('/:accountNumber', getAccount);
-accountRouter.get('/user/:userId', getUserAccounts);
-accountRouter.post('/', createAccount);
-accountRouter.put('/:accountNumber', updateAccount);
-accountRouter.delete('/:accountNumber', deleteAccount);
+accountRouter.get('/:accountNumber', authMiddleware, getAccount);
+accountRouter.get('/user/:userId', authMiddleware, getUserAccounts);
+accountRouter.post('/', authMiddleware, createAccount);
+accountRouter.put('/:accountNumber', authMiddleware, updateAccount);
+accountRouter.delete('/:accountNumber', authMiddleware, deleteAccount);
